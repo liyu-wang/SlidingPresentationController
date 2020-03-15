4a4dae55c7aacc405cb267f075cab8dde8682d93
Liyu Wang <liyuwang1985@gmail.com>
2020-03-15 16:17:41 +1100

    Added the VerticalSlidingPresentationControllerDemo Project

---
+++ VerticalSlidingPresentationControllerDemo/VerticalSlidingPresentationControllerDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VerticalSlidingPresentationControllerDemo
+//
+//  Created by Liyu Wang on 15/3/20.
+//  Copyright © 2020 Liyu Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
